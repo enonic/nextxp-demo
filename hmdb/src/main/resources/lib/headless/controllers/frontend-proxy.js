e7c9dadb36fa5d5bd75cfcccc5a6878a46409802
Espen Norderud <espen42@gmail.com>
2021-08-10 15:30:01 +0200

    Http client and frontend-proxy tweaks

--- hmdb/src/main/resources/lib/headless/controllers/frontend-proxy.js
+++ hmdb/src/main/resources/lib/headless/controllers/frontend-proxy.js
@@ -1,9 +1,9 @@
 const httpClient = require('/lib/http-client');
-const frontendOrigin = "frontendOrigin=http://localhost:3000" // <- hardcode for poc
+const frontendOrigin = "http://localhost:3000" // <- hardcode for poc
 
 const loopbackCheckParam = 'fromXp';
 
-const errorResponse = (url, status, message) => {
+const errorResponse = function(url, status, message) {
     const msg = `Failed to fetch from frontend: ${url} - ${status}: ${message}`;
     if (status >= 400) {
         log.error(msg);
@@ -18,13 +18,9 @@ const errorResponse = (url, status, message) => {
 
 // This proxies requests made directly to XP to the frontend. Normally this will
 // only be used in the portal-admin content studio previews
-const frontendProxy = (req) => {
-          return {
-            contentType: 'text/html',
-            body: `<div><h1>Success!</h1><p>Triggered the frontend proxy!</p></div>`,
-            status: 200,
-        };
-/*
+const frontendProxy = function(req) {
+
+
     const isLoopback = req.params[loopbackCheckParam];
     if (isLoopback) {
         log.info(`Loopback to XP detected from path ${req.rawPath}`);
@@ -38,10 +34,17 @@ const frontendProxy = (req) => {
     const pathStartIndex = req.rawPath.indexOf(req.branch) + req.branch.length;
 
     // this isn't likely to show up at when not working with Enonic's stuff.
-    // const contentPath = req.rawPath.slice(pathStartIndex).replace('/www.nav.no', '');
+    const contentPath = req.rawPath; //.slice(pathStartIndex).replace('/www.nav.no', '');
 
     const frontendPath = req.branch === 'draft' ? `/draft${contentPath}` : contentPath;
     const frontendUrl = `${frontendOrigin}${frontendPath}?${loopbackCheckParam}=true`;
+
+    log.info("frontendUrl (" +
+        (Array.isArray(frontendUrl) ?
+            ("array[" + frontendUrl.length + "]") :
+            (typeof frontendUrl + (frontendUrl && typeof frontendUrl === 'object' ? (" with keys: " + JSON.stringify(Object.keys(frontendUrl))) : ""))
+        ) + "): " + JSON.stringify(frontendUrl, null, 2)
+    );
 
     try {
         const response = httpClient.request({
@@ -58,7 +61,8 @@ const frontendProxy = (req) => {
             return errorResponse(frontendUrl, 500, 'No response from HTTP client');
         }
 
-        const { status, message } = response;
+        const status = response.status;
+        const message = response.message;
 
         if (status >= 400) {
             log.info(`Error response from frontend for ${frontendUrl}: ${status} - ${message}`);
@@ -70,10 +74,17 @@ const frontendProxy = (req) => {
             return errorResponse(frontendUrl, status, 'Redirects are not supported in editor view');
         }
 
-        return response;
+        //return response;
     } catch (e) {
-        return errorResponse(frontendUrl, 500, `Exception: ${e}`);
-    }*/
+        log.error(e);
+        //return errorResponse(frontendUrl, 500, `Exception: ${e}`);
+    }
+
+    return {
+        contentType: 'text/html',
+        body: `<div><h1>Success!</h1><p>Triggered the frontend proxy!</p></div>`,
+        status: 200,
+    };
 };
 
 exports.get = frontendProxy;
