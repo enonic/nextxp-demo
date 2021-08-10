b8e322065ee6d460629e8500272d65cb9566026a
Thomas Heartman <the+github@enonic.com>
2021-08-10 14:56:47 +0200

    Original idea for Next to CS interaction.

---
+++ hmdb/src/main/resources/lib/headless/controllers/frontend-proxy.js
@@ -0,0 +1,80 @@
+const httpClient = require('/lib/http-client');
+const { frontendOrigin } = "frontendOrigin=http://localhost:3000" // <- hardcode for poc
+
+const loopbackCheckParam = 'fromXp';
+
+const errorResponse = (url, status, message) => {
+    const msg = `Failed to fetch from frontend: ${url} - ${status}: ${message}`;
+    if (status >= 400) {
+        log.error(msg);
+    }
+
+    return {
+        contentType: 'text/html',
+        body: `<div>${msg}</div>`,
+        status,
+    };
+};
+
+// This proxies requests made directly to XP to the frontend. Normally this will
+// only be used in the portal-admin content studio previews
+const frontendProxy = (req) => {
+          return {
+            contentType: 'text/html',
+            body: `<div><h1>Success!</h1><p>Triggered the frontend proxy!</p></div>`,
+            status: 200,
+        };
+
+    const isLoopback = req.params[loopbackCheckParam];
+    if (isLoopback) {
+        log.info(`Loopback to XP detected from path ${req.rawPath}`);
+        return {
+            contentType: 'text/html',
+            body: `<div>Error: request to frontend looped back to XP</div>`,
+            status: 200,
+        };
+    }
+
+    const pathStartIndex = req.rawPath.indexOf(req.branch) + req.branch.length;
+
+    // this isn't likely to show up at when not working with Enonic's stuff.
+    // const contentPath = req.rawPath.slice(pathStartIndex).replace('/www.nav.no', '');
+
+    const frontendPath = req.branch === 'draft' ? `/draft${contentPath}` : contentPath;
+    const frontendUrl = `${frontendOrigin}${frontendPath}?${loopbackCheckParam}=true`;
+
+    try {
+        const response = httpClient.request({
+            url: frontendUrl,
+            contentType: 'text/html',
+            connectionTimeout: 5000,
+            headers: {
+                secret: app.config.serviceSecret,
+            },
+            followRedirects: false,
+        });
+
+        if (!response) {
+            return errorResponse(frontendUrl, 500, 'No response from HTTP client');
+        }
+
+        const { status, message } = response;
+
+        if (status >= 400) {
+            log.info(`Error response from frontend for ${frontendUrl}: ${status} - ${message}`);
+        }
+
+        // Do not send redirect-responses to the content-studio editor view,
+        // as it may cause iframe cross-origin errors
+        if (req.mode === 'edit' && status >= 300 && status < 400) {
+            return errorResponse(frontendUrl, status, 'Redirects are not supported in editor view');
+        }
+
+        return response;
+    } catch (e) {
+        return errorResponse(frontendUrl, 500, `Exception: ${e}`);
+    }
+};
+
+exports.get = frontendProxy;
+exports.handleError = frontendProxy;
