4e255ffbfec475384c94045084f2b35011023148
Espen Norderud <espen42@gmail.com>
2021-08-25 11:15:29 +0200

    #12: groundwork - move away irrelevant files

--- next/src/pages/_app.tsx
+++ next/src/pages/_app.tsx
@@ -3,8 +3,6 @@ import type {AppProps} from 'next/app'
 import Layout from '../components/blocks/layout'
 import SEO from '../components/blocks/seo'
 
-import './styles.css';
-
 function MyApp({Component, pageProps}: AppProps) {
     return (
         <Layout siteTitle="Enonic ❤ NextJS">
