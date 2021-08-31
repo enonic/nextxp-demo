e5920a1afbe4f6de49d72d4b4d15fa98894d7aa1
Espen Norderud <espen42@gmail.com>
2021-08-31 16:13:48 +0200

    #38: a little project restructuring

--- next/src/pages/[[...mainrouter]].tsx
+++ next/src/pages/[[...mainrouter]].tsx
@@ -1,13 +1,13 @@
 // this type is purposefully naive. Please make sure to update this with a more
 // accurate model before using it.
 type Context = {
-  params: { draftrouter: string[] };
+  params: { mainrouter: string[] };
 };
 
 // this function also needs some serious refactoring, but for a quick and dirty
 // proof of concept it does the job.
-export const getServerSideProps = async ({ params }: Context) => {
-  const path = "/" + params.draftrouter.join("/");
+export const getServerSideProps = async ( { params }: Context) => {
+  const path = "/" + params.mainrouter.join("/");
 
   const fallback = { displayName: "N/A", _path: "None" };
   const content = path
