f4e0532ff54ac73cce49043c2dc36c15952c0e7d
Espen Norderud <espen42@gmail.com>
2021-09-09 12:38:08 +0200

    #38: remove Folder

--- src/selectors/pageSelector.ts
+++ src/selectors/pageSelector.ts
@@ -1,15 +1,11 @@
 import { appKey } from '../enonic-connection-config';
 
-
-import FolderPage from "../components/pagetypes/Folder";
-
 // Example:
 // import MyPage from '../components/pagetypes/MyPage;
 
 // Content types mapped to top-level react components that render that content type.
 // If content type is not found here, falls back to the default page renderer in pages/Default.tsx
 export const pageSelector = {
-    //'base:folder': FolderPage
 
     // Examples:
     //'my-hardcoded-appname:my-content-type': MyPage,
--- src/selectors/querySelector.ts
+++ src/selectors/querySelector.ts
@@ -1,14 +1,11 @@
 import { appKey } from '../enonic-connection-config';
 
-import FOLDER_QUERY from "../shared/data/queries/getFolder";
-
 // Example:
 // import MY_IMPORTED_QUERY from '../shared/data/queries/getMyContentType';
 
 // Content types mapped to full guillotine query strings.
 // If type is not found here, a LOW-PERFORMING default query is selected from _getDefaultData.ts!
 export const querySelector = {
-    //'base:folder': FOLDER_QUERY,
 
     // Examples:
     // 'my.harcoded.appname:my-content-type': '{ guillotine { get { hardcoded custom query string etc } } }'
@@ -21,7 +18,6 @@ export const querySelector = {
 // idOrPath (the _id or _path of a target content item) is usually used in queries, so that's the argument the function will be called with.
 // Whenever the selected query only uses the idOrPath placeholder (including the default query), nothing needs to be added here since the default function is enough: idOrPath => ({idOrPath: idOrPath})
 export const variablesGetterSelector = {
-    //'base:folder': (idOrPath) => ({ idOrPath, maxChildren: 1000 })
 
     // EXAMPLE:
     // 'my.example.app:data-type': (idOrPath) => ({ idOrPath: idOrPath, otherPlaceHolder: otherValue })
