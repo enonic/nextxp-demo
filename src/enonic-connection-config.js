1323a6292e71ec973a4a80509f160dccef266be1
Espen Norderud <espen42@gmail.com>
2021-09-09 14:30:01 +0200

    #29: Unbind from HMDB

--- src/enonic-connection-config.js
+++ src/enonic-connection-config.js
@@ -1,5 +1,5 @@
-const project = 'hmdb';                                                     // <-- project identifier in path, e.g. 'default' in the URL <domain>/site/default/master
-const appKey = "com.example.myproject";                                     // <-- full app key = appName in hmdb/gradle.properties
+const project = 'default';                                                      // <-- project identifier in path, e.g. 'default' in the URL <domain>/site/default/master
+const appKey = "com.example.xp.myapp";                                          // <-- full app key = appName in hmdb/gradle.properties
 
 const appKeyUnderscored = appKey.replace(/\./g, '_');
 const appKeyDashed = appKey.replace(/\./g, '-');
