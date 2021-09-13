d97d4a5b7d5310964acb35e8b8a5551986b8bb68
Espen Norderud <espen42@gmail.com>
2021-09-13 16:45:59 +0200

    #34: CORS headers, open specifically to the XP server, read from enonic-connection-config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,26 @@
+var apiDomain = require('./src/enonic-connection-config').apiDomain;
+
 module.exports = {
-  reactStrictMode: true
+    reactStrictMode: true,
+    async headers() {
+        return [
+            {
+                source: '/_(master|draft)/:path*',
+                headers: [
+                    {
+                        key: 'Access-Control-Allow-Headers',
+                        value: 'Content-Type',
+                    },
+                    {
+                        key: 'Access-Control-Allow-Methods',
+                        value: 'GET, OPTIONS',
+                    },
+                    {
+                        key: 'Access-Control-Allow-Origin',
+                        value: apiDomain,
+                    },
+                ],
+            },
+        ]
+    }
 }
