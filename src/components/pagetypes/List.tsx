9017b00b7dd1bacd562e487ffcc78d9dcb1fffac
Espen Norderud <espen42@gmail.com>
2021-09-29 21:36:20 +0200

    #34: test: next.js Image remains a problem.

--- src/components/pagetypes/List.tsx
+++ src/components/pagetypes/List.tsx
@@ -1,12 +1,16 @@
 import React from "react"
+import Image from 'next/image'
 import Link from 'next/link'
 
 import { List } from "../../shared/data/queries/getList";
 import {getSiteRelativePath} from "../../shared/siteRelative/siteRelative";
 
+import xpShield from '../../public/images/xp-shield.svg';
+
 const ListPage = ( {displayName, children}: List) => {
     return (
         <>
+            <Image src={xpShield} height={200} width={130} alt="XP shield"/>
             <h1>{displayName}</h1>
             {
                 children.map((child, i) => (
