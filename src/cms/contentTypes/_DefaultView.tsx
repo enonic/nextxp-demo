4fb1262c9ad6893b9970799ccdb9c61434b2779d
Espen Norderud <espen42@gmail.com>
2021-12-21 12:27:34 +0100

    CR fix and structure improvement (#116)

--- src/cms/contentTypes/_DefaultView.tsx
+++ src/cms/contentTypes/_DefaultView.tsx
@@ -4,6 +4,8 @@ import {IS_DEV_MODE} from "../../xpAdapter/enonic-connection-config";
 
 import {PageData} from "../../xpAdapter/guillotine/fetchContent";
 import RegionsView from '../../xpAdapter/views/_Region';
+import DataDump from "../../xpAdapter/views/DataDump";
+import Empty from "../../xpAdapter/views/Empty";
 
 type ContentProps = {
     displayName: string,
@@ -13,23 +15,21 @@ type Props = {
     content: ContentProps,
     page?: PageData
 }
-
+const DefaultViewDev = ({ content, page }: Props) => {
+    return (
+        <div style={{padding: "10px"}}>
+            <h2>{content.displayName}</h2>
+            <DataDump label="content" data={content} />
+            <br />
+            <RegionsView regions={page?.regions} content={content}/>
+            <br />
+            <p style={{fontSize: ".7em", color: "#bbb"}}>_DefaultView.tsx</p>
+        </div>
+    )
+}
 
 const DefaultView = IS_DEV_MODE
-    ? (props: Props) => {
-        const { content } = props;
-        return (
-            <div style={{padding: "10px"}}>
-                <h2>{content.displayName}</h2>
-                <h5>content:</h5>
-                <pre style={{fontSize: ".8em", width:"100%", whiteSpace:"pre-wrap", wordWrap: "break-word"}}>{JSON.stringify(content, null, 2)}</pre>
-                <br />
-                <RegionsView regions={props.page?.regions} content={props.content}/>
-                <br />
-                <p style={{fontSize: ".7em", color: "#bbb"}}>_DefaultView.tsx</p>
-            </div>
-        )
-    }
-    : () => <></>;
+    ? DefaultViewDev
+    : Empty;
 
 export default DefaultView;
