afc2eccef09c98a7ea1b126122879c70e030f5d8
Espen Norderud <espen42@gmail.com>
2021-12-23 15:23:19 +0100

    Clean typesRegistration with catch-alls for default behavior

--- src/cms/typesRegistration.ts
+++ src/cms/typesRegistration.ts
@@ -1,7 +1,21 @@
-import {TypesRegistry} from '../enonicAdapter/types/TypesRegistry';
+import {TypesRegistry, CATCH_ALL_NAME} from '../enonicAdapter/types/TypesRegistry';
+
+import DefaultView from "./contentTypes/_DefaultView";
+import DefaultPartView from "./parts/_Part";
+import DefaultLayoutView from "./layouts/_Layout";
 
 // Do the required enonic types registration
 import "../enonicAdapter/types/enonicTypesRegistration";
+
+
+
+
+////////////////  Catch-alls / default behavior: rendering all types that haven't been registered specifically above, with a default view.
+// This is optional, without this null is returned instead of a rendering.
+
+TypesRegistry.addContentType(CATCH_ALL_NAME, { view: DefaultView });
+TypesRegistry.addPart(CATCH_ALL_NAME, { view: DefaultPartView });
+TypesRegistry.addLayout(CATCH_ALL_NAME, { view: DefaultLayoutView });
 
 
 
@@ -19,7 +33,6 @@ TypesRegistry.addContentType(EXAMPLE_CONTENTTYPE_NAME, {
 
 
 
-
 ///////////////// Register part types:
 
 /*
@@ -29,7 +42,6 @@ TypesRegistry.addPart(EXAMPLE_PART_NAME, {
     view: OptionalReactComponent,
 });
 */
-
 
 
 
