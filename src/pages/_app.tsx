7dc05e6dbe700291da408d4f2eda8843d2534f57
Pavel Milkevich <pmi@enonic.com>
2022-10-19 16:51:37 +0200

    Remove "revalidate" from nextjs-demo front-end #438

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -29,7 +29,6 @@ function MyApp({Component, pageProps}: AppProps<FetchContentResult>) {
         <>
             <Header
                 title="🔥 Next.XP"
-                path={pageProps.meta?.path}
                 logoUrl={getUrl('images/xp-shield.svg', pageProps.meta)}
                 meta={pageProps.meta}
             />
