7bf5880f253d3f5b9c57c7cd74c60efd1af09d20
Pavel Milkevich <pmi@enonic.com>
2023-11-24 14:53:26 +0100

    Redirect to default language #767

--- src/app/[language]/[[...contentPath]]/layout.tsx
+++ src/app/[language]/[[...contentPath]]/layout.tsx
@@ -1,5 +1,5 @@
-import Footer from '../../../../components/views/Footer';
-import Header from '../../../../components/views/Header';
+import Footer from '../../../components/views/Footer';
+import Header from '../../../components/views/Header';
 import {fetchContent, getUrl} from '@enonic/nextjs-adapter';
 import {Metadata, ResolvingMetadata} from 'next';
 import {ReactNode} from 'react';
