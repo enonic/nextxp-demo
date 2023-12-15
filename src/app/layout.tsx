09ad62def8935413c128bca6825ac6b678ea7c2e
Pavel Milkevich <pmi@enonic.com>
2023-12-15 17:35:45 +0100

    Move 404 page #786

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,44 @@
-import {PORTAL_COMPONENT_ATTRIBUTE} from '@enonic/nextjs-adapter';
+import {fetchContent, getUrl, PORTAL_COMPONENT_ATTRIBUTE} from '@enonic/nextjs-adapter';
 import {Metadata} from 'next';
 import {ReactNode} from 'react';
 
 import '../styles/globals.css';
+
 import {PageProps} from './[locale]/[[...contentPath]]/page';
+import Header from '../components/views/Header';
+import Footer from '../components/views/Footer';
 
 export type LayoutProps = {
     params: PageProps
     children: ReactNode
 }
 
-export default function RootLayout({children}: LayoutProps) {
+const title = 'Next.XP 3.0';
+
+export default async function RootLayout({params, children}: LayoutProps) {
 
     const bodyAttrs: { [key: string]: string } = {
         className: "edit",
         [PORTAL_COMPONENT_ATTRIBUTE]: "page"
     }
 
+    const {meta} = await fetchContent(params);
+
     return (
         <html lang="en">
-        <body {...bodyAttrs}>{children}</body>
+        <body {...bodyAttrs}>
+        <Header meta={meta} title={title} logoUrl={getUrl('/images/xp-shield.svg', meta)}/>
+        <main>{children}</main>
+        <Footer/>
+        </body>
         </html>
     )
 }
 
-export function generateMetadata(): Metadata {
-    return {
-        title: {
-            default: 'Next.XP 3.0',
-            template: '%s | Next.XP',
-        },
-        description: 'The React Framework for Enonic XP',
-    }
+export const metadata: Metadata = {
+    title: {
+        default: title,
+        template: '%s | Next.XP',
+    },
+    description: 'The React Framework for Enonic XP',
 }
