4a0d69e6e397c6f9995367b5452b6c4b3e46fe4b
Pavel Milkevich <pmi@enonic.com>
2024-01-15 15:51:55 +0100

    Migrate to Next.xp 3 #807

--- src/app/[locale]/error.tsx
+++ src/app/[locale]/error.tsx
@@ -1,6 +1,6 @@
 'use client' // Error components must be Client Components
 
-import {IS_DEV_MODE} from '@enonic/nextjs-adapter/utils';
+import {IS_DEV_MODE} from '@enonic/nextjs-adapter/env';
 import {useEffect} from 'react'
 import styles from '../../styles/error.module.css';
 
