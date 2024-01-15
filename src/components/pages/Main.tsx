698d36cfa2caf499b743c2780a0a6b9c6b798f29
Pavel Milkevich <pmi@enonic.com>
2024-01-15 15:51:55 +0100

    Migrate to Next.xp 3 #807

--- src/components/pages/Main.tsx
+++ src/components/pages/Main.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {PageProps} from '@enonic/nextjs-adapter/views/BasePage';
+import {PageProps} from '@enonic/nextjs-adapter/types';
 import RegionsView from '@enonic/nextjs-adapter/views/Region';
 
 const MainPage = (props: PageProps) => {
