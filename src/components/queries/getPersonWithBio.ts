76850cbf7178832dce8f20fc7c34b548810a6f96
Pavel Milkevich <pmi@enonic.com>
2024-08-27 16:24:06 +0200

    Problems with the new "rich text" chapter #959
    - macro part

--- src/components/queries/getPersonWithBio.ts
+++ src/components/queries/getPersonWithBio.ts
@@ -1,12 +1,13 @@
-import {APP_NAME_UNDERSCORED} from '@enonic/nextjs-adapter';
+import {APP_NAME_UNDERSCORED, richTextQuery} from '@enonic/nextjs-adapter';
 
-const getPerson = `
+const getPerson = () => `
 query($path:ID!){
   guillotine {
     get(key:$path) {
       displayName
       ... on ${APP_NAME_UNDERSCORED}_Person {
         data {
+          ${richTextQuery('bio')}
           dateofbirth
           photos {
            ... on media_Image {
--- src/components/views/PersonWithBio.tsx
+++ src/components/views/PersonWithBio.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import {FetchContentResult, getUrl, I18n} from '@enonic/nextjs-adapter';
 import Link from 'next/link';
+import RichTextView from '@enonic/nextjs-adapter/views/RichTextView';
 
 const Person = (props: FetchContentResult) => {
     const {displayName, data, parent} = props.data?.get as any;
-    const {photos} = data;
+    const {bio, photos} = data;
     const meta = props.meta;
 
     return (
         <>
             <div>
                 <h2>{displayName}</h2>
+                <RichTextView data={bio} meta={meta}></RichTextView>
                 {
                     photos.map((photo: any, i: number) => (
                         <img key={i}
