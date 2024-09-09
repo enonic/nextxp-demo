8ad7678508ecf30b8c41232e7aaae7ea0f3195ec
alansemenov <ase@enonic.com>
2024-09-09 13:58:17 +0200

    Improve the Person page #974

--- src/components/macros/Filmography.tsx
+++ src/components/macros/Filmography.tsx
@@ -1,13 +1,24 @@
-import type {MacroProps} from '@enonic/nextjs-adapter';
+import {MacroProps} from '@enonic/nextjs-adapter';
 import React from 'react'
+import Link from 'next/link';
 
 const FilmographyMacro = ({name, config, meta}: MacroProps) => {
-    // macro is used inside a <p> tag so we can't use any dom tags
-    return <>
-        <ins>
-            {[].concat(config.heading).map(val => <strong key={val}>{val}</strong>)}
-        </ins>
-    </>
+    // macro is used inside a <p> tag so we can't use any block tags
+    const prefix = meta.baseUrl +
+        (meta.locale && meta.locale !== meta.defaultLocale ? meta.locale + '/' : '');
+    const heading = config.heading || 'Filmography';
+
+    return config.movies.length ?
+        <>
+            <h4 className={"filmography-heading"}>{heading}</h4>
+            <ul>{
+                config.movies.map((movie: any, i: number) => (
+                    <li className={"filmography-movie"} key={i}>
+                        <Link href={prefix + movie._path}>{movie.displayName}</Link>
+                    </li>
+                ))
+            }</ul>
+        </> : null
 };
 
 export default FilmographyMacro;
