cd6796cd7e80cc0d04d5c32b12064110d15e2dbb
alansemenov <ase@enonic.com>
2024-09-09 13:58:17 +0200

    Improve the Person page #974

--- src/components/views/PersonWithBio.tsx
+++ src/components/views/PersonWithBio.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {FetchContentResult, getUrl, I18n} from '@enonic/nextjs-adapter';
 import Link from 'next/link';
 import RichTextView from '@enonic/nextjs-adapter/views/RichTextView';
+import styles from './PersonWithBio.module.css';
 
 const Person = (props: FetchContentResult) => {
     const {displayName, data, parent} = props.data?.get as any;
@@ -10,19 +11,24 @@ const Person = (props: FetchContentResult) => {
 
     return (
         <>
-            <div>
+            <div className={styles.person}>
                 <h2>{displayName}</h2>
-                <RichTextView data={bio} meta={meta}></RichTextView>
+                <RichTextView className={styles.bio} data={bio} meta={meta}></RichTextView>
                 {
-                    photos.map((photo: any, i: number) => (
-                        <img key={i}
-                             src={getUrl(photo.imageUrl, meta)}
-                             title={getTitle(photo, displayName)}
-                             alt={getTitle(photo, displayName)}
-                             width="500"
-                        />
-                    ))
+                    photos.length && <h4 className={styles.photosheader}>Photos</h4>
                 }
+                <div className={styles.photos}>
+                    {
+                        photos.map((photo: any, i: number) => (
+                            <img key={i}
+                                 src={getUrl(photo.imageUrl, meta)}
+                                 title={getTitle(photo, displayName)}
+                                 alt={getTitle(photo, displayName)}
+                                 width="500"
+                            />
+                        ))
+                    }
+                </div>
             </div>
             <p><Link href={getUrl(`/${parent._path}`, meta)}>{I18n.localize('back')}</Link></p>
         </>
