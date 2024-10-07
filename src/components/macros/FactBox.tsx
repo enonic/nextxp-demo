766050da5d92560ec2a03b7305dc096aa396e328
Pavel Milkevich <pmi@enonic.com>
2024-10-07 16:54:24 +0200

    Update rich text to use factbox macro and nextjs-adapter 3.1.0 #997

--- src/components/macros/FactBox.tsx
+++ src/components/macros/FactBox.tsx
@@ -2,9 +2,8 @@ import type {MacroProps} from '@enonic/nextjs-adapter';
 import React from 'react'
 
 import styles from './FactBox.module.css';
-import RichTextView from '@enonic/nextjs-adapter/views/RichTextView';
 
-const FactBox = ({name, config, meta}: MacroProps) => {
+const FactBox = ({name, children, config, meta}: MacroProps) => {
     // macro is used inside a <p> tag so we can't use any dom tags
     const header = config.header.length ? config.header : 'Fact Box';
     return <>
@@ -12,7 +11,7 @@ const FactBox = ({name, config, meta}: MacroProps) => {
         <ins className={styles.factbox}>
             <i className={styles.icon}/>
             <strong className={styles.header}>{header}</strong>
-            <RichTextView className={styles.body} data={config.body} meta={meta}></RichTextView>
+            {children}
         </ins>
     </>
 };
