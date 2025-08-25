a6b1845c65ee4dc25214b2945882da7685b3e400
Alan Semenov <alansemenov@users.noreply.github.com>
2025-08-25 14:25:26 +0200

    getPerson -> getPersonWithBio

--- src/components/queries/getPersonWithBio.ts
+++ src/components/queries/getPersonWithBio.ts
@@ -1,6 +1,6 @@
 import {APP_NAME_UNDERSCORED, richTextQuery} from '@enonic/nextjs-adapter';
 
-const getPerson = () => `
+const getPersonWithBio = () => `
 query($path:ID!){
   guillotine {
     get(key:$path) {
@@ -26,4 +26,4 @@ query($path:ID!){
   }
 }`;
 
-export default getPerson;
+export default getPersonWithBio;
