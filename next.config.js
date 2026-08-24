40bd37dcb75f5d73f9c7e035e24f04e35d709226
Pavel Milkevich <pmi@enonic.com>
2026-08-24 16:07:46 +0200

    Resolve allow-origin header from enonic_api url #1409

--- next.config.js
+++ next.config.js
@@ -1,44 +1,62 @@
 /** @type {import('next').NextConfig} */
 const path = require('path');
 
-function getEnonicWebpackConfig(config, {buildId, dev, isServer, defaultLoaders, nextRuntime, webpack}) {
+function getEnonicWebpackConfig(config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) {
     config.resolve.fallback = {
         ...config.resolve.fallback,
         // client-side resolution for node modules
-        fs: false
-    }
+        fs: false,
+    };
     config.resolve.alias = {
         ...config.resolve.alias,
-        "@phrases": path.resolve(__dirname, "src", "phrases"),
-    }
+        '@phrases': path.resolve(__dirname, 'src', 'phrases'),
+    };
     return config;
 }
 
+function getXpOrigin() {
+    try {
+        const url = new URL(process.env.ENONIC_API);
+        // Cast 127.0.0.1 to localhost to avoid CORS issues when running XP locally
+        return url.origin.replace('127.0.0.1', 'localhost');
+    } catch {
+        return undefined;
+    }
+}
+
 async function getEnonicHeaders() {
+
+    const xpOrigin = getXpOrigin();
+    const headers = [
+        {
+            key: 'Content-Security-Policy',
+            value: `script-src 'self' 'unsafe-eval' 'unsafe-inline';`,
+        },
+    ];
+    if (xpOrigin) {
+        headers.push(
+            {
+                key: 'Access-Control-Allow-Origin',
+                value: xpOrigin, // Can't be '*' when credentials are allowed
+            },
+            {
+                key: 'Access-Control-Allow-Credentials',
+                value: 'true', // Needed for cookies to be sent in cross-origin requests
+            },
+            {
+                key: 'Access-Control-Allow-Methods',
+                value: 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+            },
+        );
+    }
+
     return [
         {
             // Apply these headers to all routes in your application.
             source: '/:path*',
-            headers: [
-                {
-                    key: 'Content-Security-Policy',
-                    value: `script-src 'self' 'unsafe-eval' 'unsafe-inline';`
-                },
-                {
-                    key: 'Access-Control-Allow-Origin',
-                    value: 'http://localhost:8080' // Can't be '*' when credentials are allowed
-                },
-                {
-                    key: 'Access-Control-Allow-Credentials',
-                    value: 'true' // Needed for cookies to be sent in cross-origin requests
-                },
-                {
-                    key: 'Access-Control-Allow-Methods',
-                    value: 'GET, POST, PUT, DELETE, PATCH, OPTIONS'
-                }
-            ],
+            headers,
         },
-    ]
+    ];
 }
 
 const config = {
@@ -48,8 +66,8 @@ const config = {
     webpack: getEnonicWebpackConfig,
     turbopack: {
         resolveAlias: {
-            "@phrases": path.resolve(__dirname, "src", "phrases"),
-        }
+            '@phrases': path.resolve(__dirname, 'src', 'phrases'),
+        },
     },
     headers: getEnonicHeaders,
 };
